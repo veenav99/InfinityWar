0c2b1285e5f8fbbd8a156c889f013f2357bd688c
Veena Vrushi <114974799+veenav99@users.noreply.github.com>
2023-07-07 21:31:22 -0400

    Update LocateTitan.java

--- InfinityWar/src/avengers/LocateTitan.java
+++ InfinityWar/src/avengers/LocateTitan.java
@@ -2,55 +2,9 @@ package avengers;
 import java.util.Arrays;
 
 /**
- * 
  * Using the Adjacency Matrix of n vertices and starting from Earth (vertex 0), 
  * modify the edge weights using the functionality values of the vertices that each edge 
- * connects, and then determine the minimum cost to reach Titan (vertex n-1) from Earth (vertex 0).
- * 
- * Steps to implement this class main method:
- * 
- * Step 1:
- * LocateTitanInputFile name is passed through the command line as args[0]
- * Read from LocateTitanInputFile with the format:
- *    1. g (int): number of generators (vertices in the graph)
- *    2. g lines, each with 2 values, (int) generator number, (double) funcionality value
- *    3. g lines, each with g (int) edge values, referring to the energy cost to travel from 
- *       one generator to another 
- * Create an adjacency matrix for g generators.
- * 
- * Populate the adjacency matrix with edge values (the energy cost to travel from one 
- * generator to another).
- * 
- * Step 2:
- * Update the adjacency matrix to change EVERY edge weight (energy cost) by DIVIDING it 
- * by the functionality of BOTH vertices (generators) that the edge points to. Then, 
- * typecast this number to an integer (this is done to avoid precision errors). The result 
- * is an adjacency matrix representing the TOTAL COSTS to travel from one generator to another.
- * 
- * Step 3:
- * LocateTitanOutputFile name is passed through the command line as args[1]
- * Use Dijkstra’s Algorithm to find the path of minimum cost between Earth and Titan. 
- * Output this number into your output file!
- * 
- * Note: use the StdIn/StdOut libraries to read/write from/to file.
- * 
- *   To read from a file use StdIn:
- *     StdIn.setFile(inputfilename);
- *     StdIn.readInt();
- *     StdIn.readDouble();
- * 
- *   To write to a file use StdOut (here, minCost represents the minimum cost to 
- *   travel from Earth to Titan):
- *     StdOut.setFile(outputfilename);
- *     StdOut.print(minCost);
- *  
- * Compiling and executing:
- *    1. Make sure you are in the ../InfinityWar directory
- *    2. javac -d bin src/avengers/*.java
- *    3. java -cp bin avengers/LocateTitan locatetitan.in locatetitan.out
- * 
- * @author Yashas Ravi
- * 
+ * connects, and then determine the minimum cost to reach Titan (vertex n-1) from Earth (vertex 0). 
  */
 
 public class LocateTitan {
@@ -61,8 +15,6 @@ public class LocateTitan {
             StdOut.println("Execute: java LocateTitan <INput file> <OUTput file>");
             return;
         }
-
-    	// WRITE YOUR CODE HERE
 
         String inputFile = args[ 0 ];
         String outputFile = args[ 1 ];
@@ -124,8 +76,6 @@ public class LocateTitan {
 
         int[] minCost2 = dijkstra( adjacencyMatrix , 0 );
 
-
-
         for ( int i = 0; i < n - 1; i++){
 
             System.out.print( minCost2[ i ] + " \t" );
@@ -138,7 +88,7 @@ public class LocateTitan {
         StdOut.print( minCost2[ n - 1 ]);
     }   
 
-
+	
     public static int[] dijkstra( int[][] graph, int start ) {
 
         int[] space = new int[ graph.length ];
@@ -167,7 +117,6 @@ public class LocateTitan {
             if ( shortestIndex == -1 ) {
                 
                 return space;
-
             }
 
             for ( int i = 0; i < graph[shortestIndex].length; i++ ) {
